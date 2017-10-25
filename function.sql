b3059cdc60697f652bbeb6950041080496b5a993
Eiske Hashimoto <hashimoto.eiske@gmail.com>
2017-10-25 21:25:44 -0200

    Movidos

--- function.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE FUNCTION f_media_peso RETURN float IS
-media FLOAT;
-BEGIN
-  SELECT Avg(peso) INTO media FROM Producao;
-  RETURN round(media, 2);
-END;
-
-SELECT mediaPeso FROM producao;
